7920c3f13c90b124ee087f3389c89e246706626f
Laur Ivan <laur.ivan@gmail.com>
2011-12-03 02:00:35 +0100

    Added a 403 handler. Guardian should be updated too.

--- userena/middleware.py
+++ userena/middleware.py
@@ -1,9 +1,32 @@
 from django.utils import translation
 from django.core.exceptions import ObjectDoesNotExist
 from django.conf import settings
+from django.template import RequestContext, loader
 from django.contrib.auth.models import SiteProfileNotAvailable
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponseForbidden
+
+
 
 from userena import settings as userena_settings
+
+
+class Http403(Exception):
+    pass
+
+def render_to_403(*args, **kwargs):
+    """
+    Returns a HttpResponseForbidden whose content is filled with the result of calling
+    django.template.loader.render_to_string() with the passed arguments.
+    """
+    if not isinstance(args,list):
+        args = []
+        args.append('403.html')
+
+    httpresponse_kwargs = {'mimetype': kwargs.pop('mimetype', None)}
+    response = HttpResponseForbidden(loader.render_to_string(*args, **kwargs), **httpresponse_kwargs)
+
+    return response
 
 class UserenaLocaleMiddleware(object):
     """
@@ -28,3 +51,10 @@ class UserenaLocaleMiddleware(object):
                         translation.activate(lang)
                         request.LANGUAGE_CODE = translation.get_language()
                     except AttributeError: pass
+
+    def process_exception(self,request,exception):
+        if isinstance(exception,Http403):
+            if settings.DEBUG:
+                raise PermissionDenied
+            return render_to_403(context_instance=RequestContext(request, {'message': exception.message,
+                                                                           'request_path': request.path}))
--- userena/views.py
+++ userena/views.py
@@ -19,6 +19,7 @@ from userena.backends import UserenaAuthenticationBackend
 from userena.utils import signin_redirect, get_profile_model
 from userena import signals as userena_signals
 from userena import settings as userena_settings
+from userena.middleware import Http403
 
 from guardian.decorators import permission_required_or_403
 
@@ -551,7 +552,8 @@ def profile_detail(request, username, template_name='userena/profile_detail.html
                              username__iexact=username)
     profile = user.get_profile()
     if not profile.can_view_profile(request.user):
-        return HttpResponseForbidden(_("You don't have permission to view this profile."))
+        raise Http403(_("You don't have permission to view this profile."))
+        #return HttpResponseForbidden(_("You don't have permission to view this profile."))
     if not extra_context: extra_context = dict()
     extra_context['profile'] = user.get_profile()
     return direct_to_template(request,
